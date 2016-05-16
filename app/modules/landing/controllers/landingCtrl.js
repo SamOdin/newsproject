4f1fd27a936b270630b59e5bd395d29f63a4afbb
SamOdin <samodin@gmail.com>
2016-05-16 17:49:20 +0300

    16.05 - 18.00

--- app/modules/landing/controllers/landingCtrl.js
+++ app/modules/landing/controllers/landingCtrl.js
@@ -2,17 +2,15 @@
 
     angular
         .module('landingCtrl', [])
-        .controller('landingCtrl', ['$scope', landingCtrl])
-
-            function landingCtrl($scope) {
+        .controller('landingCtrl', ['$scope', 'articlesProvider', 'catalogProvider', landingCtrl])
+    
+            function landingCtrl($scope, articlesProvider, catalogProvider) {
                 $scope.randomArticles = [];
 
-                $scope.articles.$promise.then(function (responce) {
-                    for (var i = 0; i < 4; i++) {
-                        var randomNumber = (Math.floor(Math.random() * (responce.length - (responce.length / 2)) + (responce.length / 2)));
-                        $scope.randomArticles.push(responce[randomNumber]);
-                    }
-                });
+                for (var i = 0; i < 4; i++) {
+                    var randomNumber = (Math.floor(Math.random() * ($scope.articles.length - ($scope.articles.length / 2)) + ($scope.articles.length / 2)));
+                    $scope.randomArticles.push($scope.articles[randomNumber]);
+                }
 
                 console.log($scope.randomArticles)
 
